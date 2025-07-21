0fbea11f8a56e7c13f84275778f98451ee569256
Abhixak <abhishekkumar2526105@gmail.com>
2025-07-21 14:14:23 +0530

    initial commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 // import LandingVid from "../Components/LandingVid";
 import Nav from '../Components/Nav';
-import LandingVid from '../Components/landingVid';
+import LandingVid from '../Components/LandingVid';
 
 const Home = () => {
   return (

7280dad63a0c210bbba76c2bb6c9db9423db255f
Abhixak <abhishekkumar2526105@gmail.com>
2025-07-21 16:11:27 +0530

    Changes in nav

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -3,8 +3,8 @@ import Logo from "../../public/logo.png"; // Make sure this path works after bui
 
 const Nav = () => {
   return (
-    <div className="flex items-center justify-between sticky top-0 shadow-md z-10 !px-8 !py-4 bg-white">
-      <img src={Logo} alt="Logo" className="h-24" />
+    <div className="flex items-center justify-between sticky top-0 shadow-sm z-10 !px-8 !py-4 bg-white">
+      <img src={Logo} alt="Logo" className="h-26" />
 
       <ul className="flex gap-2 text-black text-[1.2em]">
         <li className="hover:text-gray-500 cursor-pointer hover:border-b relative !py-4 !px-2">Home</li>

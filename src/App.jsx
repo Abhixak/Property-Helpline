149f1c10576b99f152165569eb3e885686f78dd1
Abhixak <abhishekkumar2526105@gmail.com>
2025-07-24 14:44:08 +0530

    Added whatsapp option in contact

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import About from "./Pages/About";
 import Projects from "./Pages/Projects";
 import Contact from "./Pages/Contact";
 import ProjectDetails from "./Pages/ProjectDetails";
+import ScrollToTop from "./Components/ScrollToTop";
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
